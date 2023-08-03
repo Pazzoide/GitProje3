3714ff04cbaf1948da3aed2c23723ffaba088a59
BURHAN <Brhn.Sngl.95@outlook.com>
2023-08-03 19:59:33 +0300

    Dördüncü Adım Tamamlandı

--- src/Gun01/Galeri.java
+++ src/Gun01/Galeri.java
@@ -8,6 +8,12 @@ public class Galeri {
 
         System.out.println(arb.marka+"-"+ arb.model);
 
+        Araba arb2 = new Araba();
+        arb2.marka="Mercedes";
+        arb2.model="AMG GTR";
+
+        System.out.println(arb2.marka+"-"+arb2.model);
+
 
     }
 }
