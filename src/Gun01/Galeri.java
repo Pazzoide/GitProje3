13951a36b11e2112136a3c80ac4f2c19c676cbc7
BURHAN <Brhn.Sngl.95@outlook.com>
2023-08-03 19:39:22 +0300

    İkinci Adım Tamamlandı

--- src/Gun01/Galeri.java
+++ src/Gun01/Galeri.java
@@ -3,6 +3,8 @@ package Gun01;
 public class Galeri {
     public static void main(String[] args) {
         Araba arb = new Araba();
+        arb.marka="Porsche";
+        arb.model="911";
 
 
     }
